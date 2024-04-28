80bc97559ead1d44eb3ef383b326530dfc2ab8d7
joetraining1 <josephbagussw@gmail.com>
2024-04-28 23:54:57 +0700

    one more

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -3,7 +3,9 @@ const { EmployeeController } = require("../controllers/EmployeeController");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  return res.status(200).send("you're connected to this API");
+  return res.status(200).send({
+    message: "you are connected to this API.",
+  });
 });
 router.get("/hello", (req, res) => {
   return res.status(200).send("Hello World!");
